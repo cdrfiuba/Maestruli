23f2304e219163821ff22bfe802e3c9e7543b619
Julieta Marasco <mjuli@Wazowski>
2015-09-18 22:02:10 -0300

    Código para variar el tiempo de giro cuando se varia la velocidad de ir para adelante

---
+++ Giros/Giros.ino
@@ -0,0 +1,45 @@
+/*Definicion de los pines necesarios para el funcionamiento de los sensores de ultrasonido.*/ 
+/*Deficion de los pines necesarios para el funcionamiento del motor*/
+int kMotorLP = 9;
+int kMotorLN = 10;
+int kMotorRP = 5;
+int kMotorRN = 6;
+
+const int Vel_MotorR_adelante = 36;
+const int Vel_MotorL_adelante = 40;
+
+void setup() {
+  
+/*Inicializacion de variables, primera escritura*/ 
+
+  digitalWrite(kMotorLP, LOW);
+  digitalWrite(kMotorLN, LOW);
+  digitalWrite(kMotorRP, LOW);
+  digitalWrite(kMotorRN, LOW);
+}
+
+void loop() {
+  
+    // gira a la derecha 
+    analogWrite(kMotorLP, Vel_MotorL_adelante); 
+    analogWrite(kMotorLN, 0); 
+    analogWrite(kMotorRP, 0); 
+    analogWrite(kMotorRN, Vel_MotorR_adelante);
+    delay (625);
+    
+    // gira a la izquierda 
+    analogWrite(kMotorLP, 0); 
+    analogWrite(kMotorLN, Vel_MotorL_adelante); 
+    analogWrite(kMotorRP, Vel_MotorR_adelante); 
+    analogWrite(kMotorRN, 0);
+    delay(700);
+
+    //Stop
+    digitalWrite(kMotorLP, LOW);
+    digitalWrite(kMotorLN, LOW);
+    digitalWrite(kMotorRP, LOW);
+    digitalWrite(kMotorRN, LOW);
+    delay (2000);
+
+
+}
