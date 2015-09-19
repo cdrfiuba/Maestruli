455c0c034ecc6d1a65000cb264daf9526f8a2d04
Julieta Marasco <mjuli@Wazowski>
2015-09-18 22:03:58 -0300

    Código para variar la velocidad de cada rueda hasta lograr que vaya derecho

---
+++ Ir_para_adelante/Ir_para_adelante.ino
@@ -0,0 +1,25 @@
+int kMotorLP = 9;
+int kMotorLN = 10;
+int kMotorRP = 5;
+int kMotorRN = 6;
+/* Configuramos los pines en cero para apagar el motor */
+
+void setup(){
+
+digitalWrite(kMotorLP, LOW);
+digitalWrite(kMotorLN, LOW);
+digitalWrite(kMotorRP, LOW);
+digitalWrite(kMotorRN, LOW);
+
+}
+
+void loop(){
+
+  analogWrite(kMotorLP, 40); 
+  analogWrite(kMotorLN, 0); 
+  analogWrite(kMotorRP, 36); 
+  analogWrite(kMotorRN, 0);
+
+delay(1000);
+
+}
